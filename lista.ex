03fec53da79ec2cbdaf301311a8baeefb60f327e
fede1608 <giselleariadna@gmail.com>
2015-10-27 20:07:46 -0300

    Lista, loop iniciar

--- lista.ex
+++ lista.ex
@@ -1,2 +1,16 @@
 defmodule Lista do
+
+  def start do
+    spawn_link(fn -> loop end)
+  end
+
+  def loop do
+    receive do
+      {pid, :alumnoSeConecta} -> IO.puts 'Un alumno se conecto'
+      {pid, :profesorSeConecta} -> IO.puts 'Un Profesor se Conecto'
+      {pid, mensaje, :nuevoMensaje} -> IO.puts "Un alumno mando: " <> mensaje
+    end
+    loop
+  end
+
 end
