32df34a2cb08f0cdbd4769bbf196e4b96f8fd2eb
Francisco <francisco@gcoop.coop>
2015-10-27 20:24:25 -0300

    alumno

--- alumno.exs
+++ alumno.exs
@@ -1,3 +1,13 @@
 defmodule Alumno do
   IO.puts "Alumno: loaded."
+  def start do
+    spawn_link(fn -> loop end)
+  end
+
+  def loop do
+    receive do
+      {pid, respuesta, :nuevaRespuesta} -> IO.puts "Hay una nueva respuesta " <> respuesta
+      {pid, mensaje, :nuevoMensaje} -> IO.puts "Un alumno mando: " <> mensaje
+    end
+  end
 end
