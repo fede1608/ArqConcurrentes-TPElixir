22c196229d3203013cdf7ff45ff508d03794ff10
fede1608 <giselleariadna@gmail.com>
2015-10-27 20:29:12 -0300

    Lista Estado

--- lista.exs
+++ lista.exs
@@ -2,19 +2,28 @@ defmodule Lista do
 
   IO.puts "Lista: loaded."
   def start do
+    map=%{}
+    Map.put(map,:alumnos,[])
+    Map.put(map,:profesores,[])
+    Map.put(map,:mensajes,[])
     spawn_link(fn -> loop(%{}) end)
   end
 
   def loop(map) do
+    alumnos=Map.get(map,:alumnos)
+    profesores=Map.get(map,:profesores)
+    mensajes=Map.get(map,:mensajes)
     receive do
       {pid, :alumnoSeConecta} ->
       IO.puts 'Un alumno se conecto'
-      loop(Map.put(map, key, value))
+      Map.put(map,:alumnos,alumnos ++ [pid])
       {pid, :profesorSeConecta} ->
-      IO.puts 'Un Profesor se Conecto'
+      IO.puts 'Un profesor se conecto'
+      Map.put(map,:profesores,profesores ++ [pid])
       {pid, mensaje, :nuevoMensaje} ->
       IO.puts "Un alumno mando: " <> mensaje
     end
+    loop(map)
   end
 
 end
