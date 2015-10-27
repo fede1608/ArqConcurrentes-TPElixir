f03ebf9ef8f8612f9f7efe2990b9d4effb638dd5
fede1608 <giselleariadna@gmail.com>
2015-10-27 20:53:48 -0300

    Se manda mensajes y respuestas

--- lista.exs
+++ lista.exs
@@ -3,9 +3,9 @@ defmodule Lista do
   IO.puts "Lista: loaded."
   def start do
     map=%{}
-    Map.put(map,:alumnos,[])
-    Map.put(map,:profesores,[])
-    Map.put(map,:mensajes,[])
+    map=Map.put(map,:alumnos,[])
+    map=Map.put(map,:profesores,[])
+    map=Map.put(map,:mensajes,[])
     spawn_link(fn -> loop(map) end)
   end
 
@@ -15,15 +15,39 @@ defmodule Lista do
     mensajes=Map.get(map,:mensajes)
     receive do
       {pid, :alumnoSeConecta} ->
-      IO.puts 'Un alumno se conecto'
-      Map.put(map,:alumnos,[] ++ [pid])
+        IO.puts "Un alumno se conecto"
+        Map.put(map,:alumnos,alumnos ++ [pid])
       {pid, :profesorSeConecta} ->
-      IO.puts 'Un profesor se conecto'
-      Map.put(map,:profesores,profesores ++ [pid])
+        IO.puts "Un profesor se conecto"
+        Map.put(map,:profesores,profesores ++ [pid])
       {pid, mensaje, :nuevoMensaje} ->
-      IO.puts "Un alumno mando: " <> mensaje
+        IO.puts "Un alumno mando: " <> mensaje
+        sendNuevoMensaje(alumnos,mensaje)
+        sendNuevoMensaje(profesores,mensaje)
+      {pid, mensaje, respuesta, :profesorResponde} ->
+        IO.puts "Un profesor respondio: " <> respuesta
+        sendNuevaRespuesta(alumnos,mensaje,respuesta)
+        sendNuevaRespuesta(profesores,mensaje,respuesta)
     end
     loop(map)
+  end
+
+  def sendNuevoMensaje([head|tail],mensaje) do
+    send head,{self,mensaje,:nuevaConsulta}
+    sendNuevoMensaje(tail,mensaje)
+  end
+
+  def sendNuevoMensaje([],mensaje) do
+    IO.puts "Se termino de enviar mensaje " <> mensaje
+  end
+
+  def sendNuevaRespuesta([head|tail],mensaje,respuesta) do
+    send head,{self,mensaje,respuesta,:nuevaRespuesta}
+    sendNuevaRespuesta(tail,mensaje,respuesta)
+  end
+
+  def sendNuevaRespuesta([],mensaje,respuesta) do
+    IO.puts "Se termino de enviar respuesta " <> respuesta <> " al mensaje " <> mensaje
   end
 
 end
