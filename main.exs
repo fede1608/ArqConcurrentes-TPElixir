169659d45081f9c03d1d5d4eae8393fae69583f0
Jonathan Corallo <jonathan.corallo92@gmail.com>
2015-10-27 20:33:20 -0300

    Levantar procesos en main

--- main.exs
+++ main.exs
@@ -5,8 +5,14 @@ Code.require_file "lista.exs", __DIR__
 defmodule Main do
   IO.puts "------ Main running -------"
 #levanta lista
+  lista = Lista.start
+  IO.puts "Lista: " <> inspect(lista)
 #levanta alumno
+  alumno = Alumno.start
+  IO.puts "Alumno: " <> inspect(alumno)
 #levanta profesor
+  profesor = Profesor.start
+  IO.puts "Profesor: " <> inspect(profesor)
 #conecta alumno con lista
 #conecta profesor con lista
 end
