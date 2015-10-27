b9a9353372eec6bde8535519352418e8979d6e2e
fnc <fnc@git.com>
2015-10-27 20:47:58 -0300

    nueva consulta

--- profesor.exs
+++ profesor.exs
@@ -2,16 +2,16 @@ defmodule Profesor do
   IO.puts "Profesor: loaded."
   def start(lista) do
     send lista,{self, :profesorSeConecta}
-    spawn_link(fn -> loop(lista) end)
+    spawn_link(fn -> loop end)
   end
 
-  def loop(lista) do
+  def loop do
     receive do
-      {pid, lista, :nuevaConsulta ,consulta} -> 
+      {lista,:nuevaConsulta ,consulta} -> 
         IO.puts "Hay una nueva consulta" <> consulta
         send lista,{self,:nuevaRespuesta}
-      {pid, :profesorResponde} -> IO.puts 'Un profesor responde'
-      {pid, _ } -> send :pid, {:error, 'Accion Invalida de #{inspect pid}'}
+      {lista, :profesorResponde} -> IO.puts 'Un profesor responde'
+      {lista, _ } -> send :pid, {:error, 'Accion Invalida de #{inspect pid}'}
     end
   end
 
