311172e64a6c3af4c11dbf3d2a3f4cc72b7e830b
Jonathan Corallo <jonathan.corallo92@gmail.com>
2015-10-27 20:16:32 -0300

    Cambio de extension de archivos

--- main.exs
+++ main.exs
@@ -1,3 +1,7 @@
+Code.require_file "alumno.exs", __DIR__
+Code.require_file "profesor.exs", __DIR__
+Code.require_file "lista.exs", __DIR__
+
 defmodule Main do
   IO.puts "------ Main running -------"
 #levanta lista
